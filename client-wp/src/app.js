d5d17e07807fffaf77781c89fbf6cb2546ed2987
Ghislain B <gbeaulac@gmail.com>
2017-01-20 22:35:08 -0500

    Remove welcome vm and route

--- client-wp/src/app.js
+++ client-wp/src/app.js
@@ -2,8 +2,8 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',    moduleId: './welcome', nav: true, title: 'Welcome' },
-      { route: 'slickgrid',     name: 'slickgrid',  moduleId: './modules/slickgrid/index', nav: true, title: 'Slickgrid' }
+      { route: 'slickgrid', name: 'slickgrid', moduleId: './modules/slickgrid/index', nav: true, title: 'Slickgrid' },
+      { route: '',          redirect: 'slickgrid' }
     ]);
 
     this.router = router;
