ee178a496bf6f817f0a0acb5b05de24a129caa05
ghiscoding <gbeaulac@gmail.com>
2017-12-16 15:18:54 -0500

    add icon classes for styling purposes

--- aurelia-slickgrid/src/aurelia-slickgrid/formatters/checkmarkFormatter.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/formatters/checkmarkFormatter.ts
@@ -1,4 +1,4 @@
 import { Column, Formatter } from './../models/index';
 
 export const checkmarkFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) =>
-  value ? `<i class="fa fa-check" aria-hidden="true"></i>` : '';
+  value ? `<i class="fa fa-check checkmark-icon" aria-hidden="true"></i>` : '';
