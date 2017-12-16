5abfb1cc821a491e0fa85397291fad5ed966422f
ghiscoding <gbeaulac@gmail.com>
2017-12-16 17:38:12 -0500

    Move locales to i18n folder with namespace

--- aurelia-slickgrid/src/main.ts
+++ aurelia-slickgrid/src/main.ts
@@ -32,9 +32,11 @@ export function configure(aurelia: Aurelia) {
 
     return instance.setup({
       backend: {
-        loadPath: './locales/{{lng}}.json',
+        loadPath: './static/i18n/{{lng}}/{{ns}}.json',
       },
       lng: 'en',
+      ns: ['aurelia-slickgrid'],
+      defaultNS: 'aurelia-slickgrid',
       attributes: ['t', 'i18n'],
       fallbackLng: 'en',
       debug: false
