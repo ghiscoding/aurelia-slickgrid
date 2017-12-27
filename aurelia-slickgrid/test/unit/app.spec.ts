fed09ead149c6904063e699dca5a0d2aabc1d110
Ghislain B <gbeaulac@gmail.com>
2017-12-27 18:41:54 -0500

    update all client samples to latest lib version

--- aurelia-slickgrid/test/unit/app.spec.ts
+++ aurelia-slickgrid/test/unit/app.spec.ts
@@ -2,10 +2,8 @@ import { App } from '../../src/app';
 
 /*
 describe('the app', () => {
-  /*
   it('says hello', () => {
     expect(new App().message).toBe('Hello World!');
   });
-  */
 });
 */
--- client-ts-wp/test/unit/app.spec.ts
+++ client-ts-wp/test/unit/app.spec.ts
@@ -1,7 +1,8 @@
 import { App } from '../../src/app';
-
+/*
 describe('the app', () => {
   it('says hello', () => {
-    // expect(new App().message).toBe('Hello World!');
+    expect(new App().message).toBe('Hello World!');
   });
 });
+*/
