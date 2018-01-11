49ae942a1499fb0e2b8db7ed2dc31081364a47e7
Ghislain B <gbeaulac@gmail.com>
2018-01-11 00:09:12 -0500

    fix a possible error thrown by an undefined grid

--- aurelia-slickgrid/src/aurelia-slickgrid/services/controlAndPlugin.service.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/services/controlAndPlugin.service.ts
@@ -144,7 +144,9 @@ export class ControlAndPluginService {
           options.gridMenu.onMenuClose(e, args);
         }
         // we also want to resize the columns if the user decided to hide certain column(s)
-        this._grid.autosizeColumns();
+        if (this._grid && typeof this._grid.autosizeColumns === 'function') {
+          this._grid.autosizeColumns();
+        }
       });
     }
     return gridMenuControl;
