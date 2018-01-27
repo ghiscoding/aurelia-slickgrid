e91c5d205d4bf009b6a1b9e49128443f335ac7c9
ghiscoding <gbeaulac@gmail.com>
2018-01-27 16:44:08 -0500

    fixed error thrown by SlickGrid of "Stylesheet missing"

--- aurelia-slickgrid/src/aurelia-slickgrid/services/controlAndPlugin.service.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/services/controlAndPlugin.service.ts
@@ -188,6 +188,9 @@ export class ControlAndPluginService {
       this.columnPickerControl = null;
     }
     if (this.gridMenuControl) {
+      this.gridMenuControl.onBeforeMenuShow.unsubscribe();
+      this.gridMenuControl.onCommand.unsubscribe();
+      this.gridMenuControl.onMenuClose.unsubscribe();
       this.gridMenuControl.destroy();
       this.gridMenuControl = null;
     }
