2a6034c38ee4d5a78da5b361793695a024fcaec1
Ghislain B <gbeaulac@gmail.com>
2018-02-19 21:19:06 -0500

    fix pagination view displayed even "gridOptions.enablePagination=false"

--- aurelia-slickgrid/src/aurelia-slickgrid/global-grid-options.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/global-grid-options.ts
@@ -25,7 +25,6 @@ export const GlobalGridOptions: GridOption = {
   enableColumnPicker: true,
   enableColumnReorder: true,
   enableGridMenu: true,
-  enablePagination: false,
   enableSorting: true,
   enableTextSelectionOnCells: true,
   explicitInitialization: true,
