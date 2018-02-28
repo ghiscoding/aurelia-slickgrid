72ef747bed152fb8c2fa2f08c2739817832219ac
Ghislain B <gbeaulac@gmail.com>
2018-02-28 00:35:57 -0500

    feat(grid): add inline editor undo command

---
+++ aurelia-slickgrid/src/aurelia-slickgrid/models/editCommand.interface.ts
@@ -0,0 +1,15 @@
+import { Editor } from './editor.interface';
+
+export interface EditCommand {
+  row: number;
+  cell: number;
+  editor: Editor | any;
+  serializedValue: any;
+  prevSerializedValue: any;
+
+  /** Call to commit changes */
+  execute: () => void;
+
+  /** Call to rollback changes */
+  undo: () => void;
+}
