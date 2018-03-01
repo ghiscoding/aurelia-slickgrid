f26e5344b1cbd22270ae9f8c8bf2771b5cc26d6b
Ghislain B <gbeaulac@gmail.com>
2018-03-01 00:08:57 -0500

    delete(sample): remove unnecessary client sample `client-ts-wp`, ref #10 - both `client-ts-wp` and `doc/github-demo` use TypeScript WebPack, so if users want to see that kind of client sample, they can look at the github-demo

--- client-ts-wp/src/resources/index.ts
+++
@@ -1,8 +0,0 @@
-import { FrameworkConfiguration } from 'aurelia-framework';
-import { PLATFORM } from 'aurelia-pal';
-
-export function configure(config: FrameworkConfiguration) {
-  config.globalResources([
-    PLATFORM.moduleName('./value-converters/stringify')
-  ]);
-}
