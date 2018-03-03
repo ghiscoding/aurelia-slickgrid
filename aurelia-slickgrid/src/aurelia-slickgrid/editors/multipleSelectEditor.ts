ad2a7e30a5ba25a9b178bdad97f446d37abc0c41
Jeremy Zagorski <jmzagorski@gmail.com>
2018-03-03 13:36:51 -0500

    feat(selectEditors): add select grid editors
    select editors are common with data to ensure data integrity

---
+++ aurelia-slickgrid/src/aurelia-slickgrid/editors/multipleSelectEditor.ts
@@ -0,0 +1,180 @@
+import { arraysEqual } from '../services/index';
+import {
+  Editor,
+  Column,
+  MultipleSelectOption,
+  SelectOption
+} from './../models/index';
+import * as $ from 'jquery';
+
+/**
+ * Slickgrid editor class for multiple select lists
+ */
+export class MultipleSelectEditor implements Editor {
+  /**
+   * The JQuery DOM element
+   */
+  $filterElm: any;
+  /**
+   * The slick grid column being edited
+   */
+  columnDef: Column;
+  /**
+   * The multiple-select options for a multiple select list
+   */
+  defaultOptions: MultipleSelectOption;
+  /**
+   * The default item values that are set
+   */
+  defaultValue: any[];
+  /**
+   * The options label/value object to use in the select list
+   */
+  optionCollection: SelectOption[] = [];
+  /**
+   * The property name for values in the optionCollection
+   */
+  valueName: string;
+  /**
+   * The property name for labels in the optionCollection
+   */
+  labelName: string;
+
+  constructor(private args: any) {
+    this.defaultOptions = {
+      container: 'body',
+      filter: false,
+      maxHeight: 200,
+      width: '100%',
+      okButton: true,
+      addTitle: true,
+      selectAllDelimiter: ['', '']
+    };
+
+    this.init();
+  }
+
+  /**
+   * The current selected values from the optionCollection
+   */
+  get currentValues() {
+    return this.optionCollection
+      .filter(c => this.$filterElm.val().indexOf(c[this.valueName].toString()) !== -1)
+      .map(c => c[this.valueName]);
+  }
+
+  init() {
+    if (!this.args) {
+      throw new Error('[Aurelia-SlickGrid] A filter must always have an "init()" ' +
+        'with valid arguments.');
+    }
+
+    this.columnDef = this.args.column;
+
+    const filterTemplate = this.buildTemplateHtmlString();
+
+    this.createDomElement(filterTemplate);
+  }
+
+  applyValue(item: any, state: any): void {
+    item[this.args.column.field] = state;
+  }
+
+  destroy() {
+    this.$filterElm.remove();
+  }
+
+  loadValue(item: any): void {
+    // convert to string because that is how the DOM will return these values
+    this.defaultValue = item[this.columnDef.field].map((i: any) => i.toString());
+
+    this.$filterElm.find('option').each((i: number, $e: any) => {
+      if (this.defaultValue.indexOf($e.value) !== -1) {
+        $e.selected = true;
+      } else {
+        $e.selected = false;
+      }
+    });
+
+    this.refresh();
+  }
+
+  serializeValue(): any {
+    return this.currentValues;
+  }
+
+  focus() {
+    this.$filterElm.focus();
+  }
+
+  isValueChanged(): boolean {
+    return !arraysEqual(this.$filterElm.val(), this.defaultValue);
+  }
+
+  validate() {
+    if (this.args.column.validator) {
+      const validationResults = this.args.column.validator(this.currentValues, this.args);
+      if (!validationResults.valid) {
+        return validationResults;
+      }
+    }
+
+    return {
+      valid: true,
+      msg: null
+    };
+  }
+
+  private buildTemplateHtmlString() {
+    if (!this.columnDef || !this.columnDef.filter || !this.columnDef.filter.collection) {
+      throw new Error('[Aurelia-SlickGrid] You need to pass a "collection" for ' +
+        'the MultipleSelect Filter to work correctly. Also each option should include ' +
+        'a value/label pair (or value/labelKey when using Locale). For example:: ' +
+        '{ filter: type: FilterType.multipleSelect, collection: [{ value: true, label: \'True\' }, ' +
+        '{ value: false, label: \'False\'}] }');
+    }
+    this.optionCollection = this.columnDef.filter.collection || [];
+    this.labelName = (this.columnDef.filter.customStructure) ? this.columnDef.filter.customStructure.label : 'label';
+    this.valueName = (this.columnDef.filter.customStructure) ? this.columnDef.filter.customStructure.value : 'value';
+
+    let options = '';
+    this.optionCollection.forEach((option: SelectOption) => {
+      if (!option || (option[this.labelName] === undefined && option.labelKey === undefined)) {
+        throw new Error('A collection with value/label (or value/labelKey when using ' +
+          'Locale) is required to populate the Select list, for example:: { filter: ' +
+          'type: FilterType.multipleSelect, collection: [ { value: \'1\', label: \'One\' } ])');
+      }
+      const labelKey = (option.labelKey || option[this.labelName]) as string;
+      const textLabel = labelKey;
+
+      options += `<option value="${option[this.valueName]}">${textLabel}</option>`;
+    });
+
+    return `<select class="ms-filter search-filter" multiple="multiple">${options}</select>`;
+  }
+
+  private createDomElement(filterTemplate: string) {
+    this.$filterElm = $(filterTemplate);
+
+    if (this.$filterElm && typeof this.$filterElm.appendTo === 'function') {
+      this.$filterElm.appendTo(this.args.container);
+    }
+
+    if (typeof this.$filterElm.multipleSelect !== 'function') {
+      // fallback to bootstrap
+      this.$filterElm.addClass('form-control');
+    } else {
+      const filterOptions = (this.columnDef.filter) ? this.columnDef.filter.filterOptions : {};
+      const options: MultipleSelectOption = { ...this.defaultOptions, ...filterOptions };
+      this.$filterElm = this.$filterElm.multipleSelect(options);
+    }
+  }
+
+  // refresh the jquery object because the selected checkboxes were already set
+  // prior to this method being called
+  private refresh() {
+    if (typeof this.$filterElm.multipleSelect === 'function') {
+      this.$filterElm.data('multipleSelect').refresh();
+    }
+  }
+}
--- aurelia-slickgrid/src/aurelia-slickgrid/formatters/arrayToCsvFormatter.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/formatters/arrayToCsvFormatter.ts
@@ -2,7 +2,8 @@ import { Column, Formatter } from './../models/index';
 
 export const arrayToCsvFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
   if (value && Array.isArray(value)) {
-    return value.join(', ');
+    const values = value.join(', ');
+    return `<span title="${values}">${values}</span>`;
   }
   return '';
 };
