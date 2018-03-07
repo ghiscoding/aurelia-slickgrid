52de9786bd7ff1f45e8da106482e523a17919691
Ghislain B <gbeaulac@gmail.com>
2018-03-06 23:38:37 -0500

    feat(formatter): add a new HyperlinkURI formatter, you need to uriPrefix

--- aurelia-slickgrid/src/aurelia-slickgrid/formatters/hyperlinkFormatter.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/formatters/hyperlinkFormatter.ts
@@ -1,9 +1,11 @@
 import { Column, Formatter } from './../models/index';
 
 export const hyperlinkFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
-  const matchUrl = value.match(/^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/, 'i');
-  if (matchUrl && Array.isArray(matchUrl)) {
-    return `<a href="${matchUrl[0]}">' + value + '</a>`;
+  if (value && typeof value === 'string') {
+    const matchUrl = value.match(/^(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/i);
+    if (matchUrl && Array.isArray(matchUrl)) {
+      return `<a href="${matchUrl[0]}">' + value + '</a>`;
+    }
   }
   return '';
 };
---
+++ aurelia-slickgrid/src/aurelia-slickgrid/formatters/hyperlinkUriPrefixFormatter.ts
@@ -0,0 +1,11 @@
+import { Column, Formatter } from './../models/index';
+
+/** Takes an hyperlink URI prefix (passed in column definition "params.uriPrefix") and adds the cell value. The structure will be "<a href="uriPrefix">value</a>"  */
+export const hyperlinkUriPrefixFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
+  let uriPrefix = (columnDef && columnDef.params && columnDef.params.uriPrefix) ? columnDef.params.uriPrefix : '';
+  if (value && uriPrefix && typeof uriPrefix === 'string' && !uriPrefix.includes('<script>')) {
+    uriPrefix += value;
+    return '<a href="' + uriPrefix + '">' + value + '</a>';
+  }
+  return '';
+};
