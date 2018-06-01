17db907a22bfc79075909dfad8ce8e0f2ee5550f
Ghislain B <gbeaulac@gmail.com>
2018-05-31 22:32:37 -0400

    fix(sorter): issue #72 circular dependency from last PR #70

--- aurelia-slickgrid/src/aurelia-slickgrid/sorters/sorterUtilities.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/sorters/sorterUtilities.ts
@@ -1,6 +1,5 @@
 import { FieldType } from './../models/fieldType.enum';
 import { Sorters } from './index';
-import * as moment from 'moment';
 
 export function sortByFieldType(value1: any, value2: any, fieldType: FieldType, sortDirection: number) {
   let sortResult = 0;
@@ -27,20 +26,4 @@ export function sortByFieldType(value1: any, value2: any, fieldType: FieldType, 
   }
 
   return sortResult;
-}
-
-export function compareDates(sortDirection: number, value1: any, value2: any, format: string | moment.MomentBuiltinFormat, strict?: boolean) {
-  let diff = 0;
-
-  if (value1 === null || value1 === '' || !moment(value1, format, strict).isValid()) {
-    diff = -1;
-  } else if (value2 === null || value2 === '' || !moment(value2, format, strict).isValid()) {
-    diff = 1;
-  } else {
-    const date1 = moment(value1, format, strict);
-    const date2 = moment(value2, format, strict);
-    diff = parseInt(date1.format('X'), 10) - parseInt(date2.format('X'), 10);
-  }
-
-  return sortDirection * (diff === 0 ? 0 : (diff > 0 ? 1 : -1));
 }
