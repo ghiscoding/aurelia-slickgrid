d1994887f9b45c2f3650cef3ce24742855ef40ec
Ghislain B <gbeaulac@gmail.com>
2018-06-06 10:35:53 -0400

    fix(sort): Presets & Sort caused unintended behavior w/dataview changed (#76)
    * fix(sort): Presets & Sort caused unintended behavior w/dataview changed
    * refactor(event): change dispatch event into 1 liner with private function

--- aurelia-slickgrid/src/aurelia-slickgrid/services/sort.service.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/services/sort.service.ts
@@ -123,15 +123,6 @@ export class SortService {
       this.onLocalSortChanged(grid, dataView, sortColumns);
       this.emitSortChanged('local');
     });
-
-    if (dataView && dataView.onRowCountChanged) {
-      this._eventHandler.subscribe(dataView.onRowCountChanged, (e: Event, args: any) => {
-        // load any presets if there are any
-        if (args.current > 0) {
-          this.loadLocalPresets(grid, dataView);
-        }
-      });
-    }
   }
 
   /**
@@ -160,7 +151,6 @@ export class SortService {
 
     // set current sorter to empty & emit a sort changed event
     this._currentLocalSorters = [];
-    const sender = (this._gridOptions && this._gridOptions.backendServiceApi) ? 'remote' : 'local';
 
     // emit an event when filters are all cleared
     this.ea.publish('sortService:sortCleared', this._currentLocalSorters);
