8f67cd9b13626f689dd1b44e94a76d000e456c94
ghiscoding <gbeaulac@gmail.com>
2018-07-07 13:08:59 -0400

    fix(formatter): Date Formatters should only transform on valid dates - we will also return the original date when invalid, so that say we input a text instead of a date, we will see the text without the formatter throwing error

--- aurelia-slickgrid/src/aurelia-slickgrid/formatters/dateIsoFormatter.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/formatters/dateIsoFormatter.ts
@@ -3,5 +3,7 @@ import { mapMomentDateFormatWithFieldType } from './../services/utilities';
 import * as moment from 'moment';
 const FORMAT = mapMomentDateFormatWithFieldType(FieldType.dateIso);
 
-export const dateIsoFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) =>
-  value ? moment(value).format(FORMAT) : '';
+export const dateIsoFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any) => {
+  const isDateValid = moment(value, FORMAT, true).isValid();
+  return (value && isDateValid) ? moment(value).format(FORMAT) : value;
+};
