824af29fd8151d94a956fbc09e0e13f735e2a696
ghiscoding <gbeaulac@gmail.com>
2018-11-24 13:52:19 -0500

    feat(update): add updateDataGridItems for multiple changes - also provide optional flag to disable highlight on both updateDataGridItem & updateDataGridItems

--- aurelia-slickgrid/src/aurelia-slickgrid/services/grid.service.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/services/grid.service.ts
@@ -102,45 +102,6 @@ export class GridService {
     };
   }
 
-  /**
-   * Highlight then fade a row for x seconds.
-   * The implementation follows this SO answer: https://stackoverflow.com/a/19985148/1212166
-   * @param rowNumber
-   * @param fadeDelay
-   */
-  highlightRow(rowNumber: number, fadeDelay: number = 1500) {
-    // create a SelectionModel if there's not one yet
-    if (!this._grid.getSelectionModel()) {
-      const rowSelectionPlugin = new Slick.RowSelectionModel(this._gridOptions.rowSelectionOptions || {});
-      this._grid.setSelectionModel(rowSelectionPlugin);
-    }
-
-    this._grid.setSelectedRows([rowNumber]);
-    this._dataView.getItemMetadata = this.getItemRowMetadataToHighlight(this._dataView.getItemMetadata);
-
-    const item = this._dataView.getItem(rowNumber);
-    if (item && item.id) {
-      item.rowClass = 'highlight';
-      this._dataView.updateItem(item.id, item);
-
-      // highlight the row for a user defined timeout
-      $(`#${this._gridOptions.gridId}`)
-        .find(`.highlight.row${rowNumber}`)
-        .first();
-
-      // delete the row's CSS that was attached for highlighting
-      setTimeout(() => {
-        if (item && item.id) {
-          delete item.rowClass;
-          const gridIdx = this._dataView.getIdxById(item.id);
-          if (gridIdx !== undefined) {
-            this._dataView.updateItem(item.id, item);
-          }
-        }
-      }, fadeDelay + 10);
-    }
-  }
-
   /** Get the Data Item from a grid row index */
   getDataItemByRowIndex(index: number): any {
     if (!this._grid || typeof this._grid.getDataItem !== 'function') {
@@ -180,6 +141,55 @@ export class GridService {
 
     const selectedRowIndexes = this._grid.getSelectedRows();
     return this.getDataItemByRowIndexes(selectedRowIndexes);
+  }
+
+  /**
+   * Highlight then fade a row for x seconds.
+   * The implementation follows this SO answer: https://stackoverflow.com/a/19985148/1212166
+   * @param rowNumber
+   * @param fadeDelay
+   */
+  highlightRow(rowNumber: number | number[], fadeDelay: number = 1500) {
+    // create a SelectionModel if there's not one yet
+    if (!this._grid.getSelectionModel()) {
+      const rowSelectionPlugin = new Slick.RowSelectionModel(this._gridOptions.rowSelectionOptions || {});
+      this._grid.setSelectionModel(rowSelectionPlugin);
+    }
+
+    const rowIndexes = Array.isArray(rowNumber) ? rowNumber : [rowNumber];
+    this._grid.setSelectedRows(rowIndexes);
+    if (Array.isArray(rowNumber)) {
+      rowNumber.forEach(row => this.highlightRowByMetadata(row, fadeDelay));
+    } else {
+      this.highlightRowByMetadata(rowNumber, fadeDelay);
+    }
+  }
+
+  highlightRowByMetadata(rowNumber: number, fadeDelay: number = 1500) {
+    this._dataView.getItemMetadata = this.getItemRowMetadataToHighlight(this._dataView.getItemMetadata);
+
+    const item = this._dataView.getItem(rowNumber);
+    if (item && item.id) {
+      item.rowClass = 'highlight';
+      this._dataView.updateItem(item.id, item);
+      const gridOptions = this._grid.getOptions() as GridOption;
+
+      // highlight the row for a user defined timeout
+      $(`#${gridOptions.gridId}`)
+        .find(`.highlight.row${rowNumber}`)
+        .first();
+
+      // delete the row's CSS that was attached for highlighting
+      setTimeout(() => {
+        if (item && item.id) {
+          delete item.rowClass;
+          const gridIdx = this._dataView.getIdxById(item.id);
+          if (gridIdx !== undefined) {
+            this._dataView.updateItem(item.id, item);
+          }
+        }
+      }, fadeDelay + 10);
+    }
   }
 
   /** Select the selected row by a row index */
@@ -294,15 +304,37 @@ export class GridService {
   /**
    * Update an existing item with new properties inside the datagrid
    * @param object item: item object holding all properties of that row
+   * @return grid row index
    */
-  updateDataGridItem(item: any) {
+  updateDataGridItem(item: any, shouldHighlightRow = true) {
     const itemId = (!item || !item.hasOwnProperty('id')) ? undefined : item.id;
 
     if (itemId === undefined) {
       throw new Error(`Could not find the item in the grid or it's associated "id"`);
     }
 
-    this.updateDataGridItemById(itemId, item);
+    return this.updateDataGridItemById(itemId, item, shouldHighlightRow);
+  }
+
+  /**
+   * Update an array of existing items with new properties inside the datagrid
+   * @param object item: item object holding all properties of that row
+   */
+  updateDataGridItems(items: any[], shouldHighlightRow = true) {
+    if (!Array.isArray(items)) {
+      throw new Error('The function "updateDataGridItems" only support array of items, if you wish to only update 1 item then use "updateDataGridItem"');
+    }
+
+    const gridIndexes: number[] = [];
+    items.forEach((item) => {
+      gridIndexes.push(this.updateDataGridItem(item, false));
+    });
+
+    // only highlight at the end, all at once
+    // we have to do this because doing highlight 1 by 1 would only re-select the last highlighted row which is wrong behavior
+    if (shouldHighlightRow) {
+      this.highlightRow(gridIndexes);
+    }
   }
 
   /**
@@ -310,8 +342,9 @@ export class GridService {
    * @param itemId: item unique id
    * @param object item: item object holding all properties of that row
    * @param shouldHighlightRow do we want to highlight the row after update
+   * @return grid row index
    */
-  updateDataGridItemById(itemId: number | string, item: any, shouldHighlightRow = true) {
+  updateDataGridItemById(itemId: number | string, item: any, shouldHighlightRow = true): number {
     if (itemId === undefined) {
       throw new Error(`Cannot update a row without a valid "id"`);
     }
@@ -333,6 +366,8 @@ export class GridService {
 
       // refresh dataview & grid
       this._dataView.refresh();
+
+      return gridIdx;
     }
   }
 }
