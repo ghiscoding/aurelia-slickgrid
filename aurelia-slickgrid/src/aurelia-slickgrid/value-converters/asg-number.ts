87ad6cc0c8edb0f0f824803c21629865ad6c2d4f
ghiscoding <gbeaulac@gmail.com>
2018-12-20 18:18:51 -0500

    fix(converter): add prefix to value converter to avoid conflicts - prefixing the value converter with asg so that it doesn't conflict with converter possibly defined in user's code as well

--- aurelia-slickgrid/src/aurelia-slickgrid/value-converters/asg-number.ts
+++ aurelia-slickgrid/src/aurelia-slickgrid/value-converters/asg-number.ts
@@ -1,4 +1,4 @@
-export class NumberValueConverter {
+export class AsgNumberValueConverter {
   fromView(value: any, format: string): number {
     const number = parseFloat(value);
     return isNaN(number) ? value : number;
