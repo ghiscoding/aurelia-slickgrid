1d87e0f6b6dac1b4741f62ee968fdb0812d1e6d1
ghiscoding <gbeaulac@gmail.com>
2019-01-05 14:41:52 -0500

    refactor(example): comment out some of the unnecessary placeholders

--- aurelia-slickgrid/src/examples/slickgrid/example3.ts
+++ aurelia-slickgrid/src/examples/slickgrid/example3.ts
@@ -279,7 +279,7 @@ export class Example3 {
         },
         filter: {
           model: Filters.autoComplete,
-          placeholder: '&#128269; search city',
+          // placeholder: '&#128269; search city',
 
           // We can use the autocomplete through 3 ways "collection", "collectionAsync" or with your own autocomplete options
           // collectionAsync: this.httpFetch.fetch(URL_COUNTRIES_COLLECTION),
