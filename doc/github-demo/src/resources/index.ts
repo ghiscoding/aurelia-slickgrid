bfbb5c9378212457c5d2dc3cdc29902021016964
Ghislain B <gbeaulac@gmail.com>
2019-04-06 00:02:10 -0400

    refactor(folder): reorg lib folder to be in src (#153)
    - delete all client demos except the Dev one for the lib itself
    - fixed all Jest VSCode Tasks and Launch

--- doc/github-demo/src/resources/index.ts
+++
@@ -1,8 +0,0 @@
-import { FrameworkConfiguration, PLATFORM } from 'aurelia-framework';
-
-export function configure(config: FrameworkConfiguration) {
-  config.globalResources([
-    PLATFORM.moduleName('./value-converters/date-format'),
-    PLATFORM.moduleName('./value-converters/stringify')
-  ]);
-}
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+
+import { autoinject, PLATFORM } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
+
+@autoinject()
+export class App {
+  router: Router;
+  title = 'Aurelia-Slickgrid';
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = this.title;
+    config.options.pushState = true;
+    config.map([
+      { route: 'home', name: 'home', title: 'Home', moduleId: PLATFORM.moduleName('./examples/home'), nav: true, settings: { icon: 'fa fa-home' } },
+      { route: 'slickgrid', name: 'slickgrid', title: 'SlickGrid Examples', moduleId: PLATFORM.moduleName('./examples/slickgrid/index'), nav: true },
+      { route: '', redirect: 'slickgrid' }
+    ]);
+
+    this.router = router;
+  }
+}
