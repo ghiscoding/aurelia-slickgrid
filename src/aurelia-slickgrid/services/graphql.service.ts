98e8199e294426b0324e050ff0e4dfc484f1d2ce
ghiscoding <gbeaulac@gmail.com>
2019-04-06 15:30:13 -0400

    fix(backend): Filter presets is getting removed when calling sort - for BackendServiceApi (GraphQL/OData) - basically if user had Filters as preset (or from local storage) and he clicked on a Sort (without changing his Filters), it was deleting Filters from the grid state. The problem was that the Filters were executed in the grid but not saved internally and whenever users were sorting, it was returning null

--- src/aurelia-slickgrid/services/graphql.service.ts
+++ src/aurelia-slickgrid/services/graphql.service.ts
@@ -31,9 +31,9 @@ const DEFAULT_PAGE_SIZE = 20;
 
 @singleton(true)
 export class GraphqlService implements BackendService {
-  private _currentFilters: ColumnFilters | CurrentFilter[];
+  private _currentFilters: ColumnFilters | CurrentFilter[] = [];
   private _currentPagination: CurrentPagination;
-  private _currentSorters: CurrentSorter[];
+  private _currentSorters: CurrentSorter[] = [];
   private _columnDefinitions: Column[];
   private _grid: any;
   options: GraphqlServiceOption;
@@ -334,6 +334,11 @@ export class GraphqlService implements BackendService {
   updateFilters(columnFilters: ColumnFilters | CurrentFilter[], isUpdatedByPreset: boolean) {
     const searchByArray: GraphqlFilteringOption[] = [];
     let searchValue: string | string[];
+
+    // on filter preset load, we need to keep current filters
+    if (isUpdatedByPreset) {
+      this._currentFilters = this.castFilterToColumnFilter(columnFilters);
+    }
 
     for (const columnId in columnFilters) {
       if (columnFilters.hasOwnProperty(columnId)) {
