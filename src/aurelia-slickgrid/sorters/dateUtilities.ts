32936d255a163e5515e484954224b7c73690647b
Ghislain <gbeaulac@gmail.com>
2019-08-17 18:53:25 -0400

    feat(sorters): consolidate & provide all date sorters

--- src/aurelia-slickgrid/sorters/dateUtilities.ts
+++ src/aurelia-slickgrid/sorters/dateUtilities.ts
@@ -1,3 +1,5 @@
+import { mapMomentDateFormatWithFieldType } from '../services/utilities';
+import { FieldType, Sorter } from '../models/index';
 import * as moment from 'moment-mini';
 
 export function compareDates(value1: any, value2: any, sortDirection: number, format: string | moment.MomentBuiltinFormat, strict?: boolean) {
@@ -14,4 +16,16 @@ export function compareDates(value1: any, value2: any, sortDirection: number, fo
   }
 
   return sortDirection * (diff === 0 ? 0 : (diff > 0 ? 1 : -1));
+}
+
+/** From a FieldType, return the associated date Sorter */
+export function getAssociatedDateSorter(fieldType: FieldType): Sorter {
+  const FORMAT = (fieldType === FieldType.date) ? moment.ISO_8601 : mapMomentDateFormatWithFieldType(fieldType);
+
+  return (value1: any, value2: any, sortDirection: number) => {
+    if (FORMAT === moment.ISO_8601) {
+      return compareDates(value1, value2, sortDirection, FORMAT, false);
+    }
+    return compareDates(value1, value2, sortDirection, FORMAT, true);
+  };
 }
