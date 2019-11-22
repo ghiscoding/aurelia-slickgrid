0109839998385c753f116ecab5cdeb1b9b28c571
ghiscoding <gbeaulac@gmail.com>
2019-11-22 13:04:26 -0500

    refactor(examples): make UI look a bit better for new features

--- src/examples/slickgrid/example23.ts
+++ src/examples/slickgrid/example23.ts
@@ -238,7 +238,7 @@ export class Example23 {
     // we can Set Filters Dynamically (or different filters) afterward through the FilterService
     this.aureliaGrid.filterService.updateFilters([
       { columnId: 'duration', searchTerms: ['14..78'], operator: 'RangeInclusive' },
-      { columnId: 'complete', operator: 'RangeExclusive', searchTerms: [10, 80] },
+      { columnId: 'complete', operator: 'RangeExclusive', searchTerms: [15, 85] },
       { columnId: 'finish', operator: 'RangeInclusive', searchTerms: [presetLowestDay, presetHighestDay] },
     ]);
   }
