7d84cecb3767155d385487b5a6a2710ffb48e835
ghiscoding <gbeaulac@gmail.com>
2019-11-22 23:29:12 -0500

    refactor(example): cleanup code to use dropdown for dynamic filter set

--- src/examples/slickgrid/example23.ts
+++ src/examples/slickgrid/example23.ts
@@ -267,11 +267,11 @@ export class Example23 {
     this.i18n.setLocale(nextLocale).then(() => this.selectedLanguage = nextLocale);
   }
 
-  usePredefinedFilter() {
+  predefinedFilterChangedChanged(newPredefinedFilter) {
     let filters = [];
     const currentYear = moment().year();
-    console.log(this.selectedPredefinedFilter)
-    switch (this.selectedPredefinedFilter) {
+
+    switch (newPredefinedFilter) {
       case 'currentYearTasks':
         filters = [
           { columnId: 'finish', operator: OperatorType.rangeInclusive, searchTerms: [`${currentYear}-01-01`, `${currentYear}-12-31`] },
