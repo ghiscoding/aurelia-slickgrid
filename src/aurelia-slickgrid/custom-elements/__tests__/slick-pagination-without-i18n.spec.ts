32964a6d26d70d6ea24af7268a91165aa135e733
ghiscoding <gbeaulac@gmail.com>
2020-01-20 18:29:25 -0500

    (tests): try fixing Jest unit test open handle not closing

--- src/aurelia-slickgrid/custom-elements/__tests__/slick-pagination-without-i18n.spec.ts
+++ src/aurelia-slickgrid/custom-elements/__tests__/slick-pagination-without-i18n.spec.ts
@@ -77,15 +77,18 @@ describe('Slick-Pagination Component without I18N', () => {
       jest.clearAllMocks();
     });
 
-    xit('should throw an error when "enableTranslate" is set and I18N Service is not provided', async () => {
+    it('should throw an error when "enableTranslate" is set and I18N Service is not provided', async () => {
       try {
+        component.enableTranslate = true;
         await component.manuallyHandleLifecycle().create(bootstrap);
+        await component.bind({ enableTranslate: true, paginationService: paginationServiceStub });
+        await component.attached();
+        await component.unbind();
         await component.bind({ enableTranslate: true, paginationService: paginationServiceStub });
         await component.attached();
         await component.detached();
       } catch (e) {
         expect(e.toString()).toContain('[Aurelia-Slickgrid] requires "I18N" to be installed and configured when the grid option "enableTranslate" is enabled.');
-        component.dispose();
       }
     });
 
