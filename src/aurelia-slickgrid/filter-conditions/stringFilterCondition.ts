0efe8cbb350303f909c836599ad3da190ef70a67
ghiscoding <gbeaulac@gmail.com>
2020-04-02 20:31:01 -0400

    fix(filter): string filter should also work when using Contains - when using OperatorType.empty or OperatorType.contains, they should do a comparison of passing when item contains the substring of search term

--- src/aurelia-slickgrid/filter-conditions/stringFilterCondition.ts
+++ src/aurelia-slickgrid/filter-conditions/stringFilterCondition.ts
@@ -1,4 +1,4 @@
-import { FilterCondition, FilterConditionOption } from '../models/index';
+import { FilterCondition, FilterConditionOption, OperatorType } from '../models/index';
 import { testFilterCondition } from './filterUtilities';
 
 export const stringFilterCondition: FilterCondition = (options: FilterConditionOption) => {
@@ -12,11 +12,11 @@ export const stringFilterCondition: FilterCondition = (options: FilterConditionO
     searchTerm = searchTerm.toLowerCase();
   }
 
-  if (options.operator === '*' || options.operator === 'EndsWith') {
+  if (options.operator === '*' || options.operator === OperatorType.endsWith) {
     return cellValue.endsWith(searchTerm);
-  } else if ((options.operator === '' && options.cellValueLastChar === '*') || options.operator === 'StartsWith') {
+  } else if ((options.operator === '' && options.cellValueLastChar === '*') || options.operator === OperatorType.startsWith) {
     return cellValue.startsWith(searchTerm);
-  } else if (options.operator === '') {
+  } else if (options.operator === '' || options.operator === OperatorType.contains) {
     return (cellValue.indexOf(searchTerm) > -1);
   }
   return testFilterCondition(options.operator || '==', cellValue, searchTerm);
