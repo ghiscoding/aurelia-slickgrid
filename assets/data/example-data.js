adccd1bee09b0d2d72c2bfa81b676f05daccc842
Ghislain B <gbeaulac@gmail.com>
2020-05-29 13:22:21 -0400

    fix(lint): adhere to strict triple equality check (#355)

--- assets/data/example-data.js
+++ assets/data/example-data.js
@@ -1,6 +1,6 @@
 const data = [];
 
-for (let i = 0; i < 500; i++){
+for (let i = 0; i < 500; i++) {
   const d = (data[i] = {});
 
   d.id = i;
@@ -10,7 +10,7 @@ for (let i = 0; i < 500; i++){
   d['percentComplete'] = Math.round(Math.random() * 100);
   d['start'] = '01/01/2009';
   d['finish'] = '01/05/2009';
-  d['effortDriven'] = (i % 5 == 0);
+  d['effortDriven'] = (i % 5 === 0);
 }
 
 export default data;
