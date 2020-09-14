659304dde27a9fad9d099ac19c8d2e727faf98e0
Ghislain B <gbeaulac@gmail.com>
2020-09-14 19:34:45 -0400

    feat(editors): add Clear Date button to Date Editor (#418)
    * feat(editors): add Clear Date button to Date Editor

--- src/aurelia-slickgrid/editors/dateEditor.ts
+++ src/aurelia-slickgrid/editors/dateEditor.ts
@@ -1,5 +1,12 @@
 import { inject, Optional } from 'aurelia-framework';
 import { I18N } from 'aurelia-i18n';
+import * as moment from 'moment-mini';
+import * as _flatpickr from 'flatpickr';
+import * as $ from 'jquery';
+import { BaseOptions as FlatpickrBaseOptions } from 'flatpickr/dist/types/options';
+import { FlatpickrFn } from 'flatpickr/dist/types/instance';
+const flatpickr: FlatpickrFn = _flatpickr as any; // patch for rollup
+
 import { Constants } from './../constants';
 import { mapFlatpickrDateFormatWithFieldType, mapMomentDateFormatWithFieldType, setDeepValue, getDescendantProperty } from './../services/utilities';
 import {
@@ -13,9 +20,6 @@ import {
   FlatpickrOption,
   GridOption,
 } from './../models/index';
-import * as flatpickr from 'flatpickr';
-import * as moment from 'moment-mini';
-import * as $ from 'jquery';
 
 declare function require(name: string): any;
 
@@ -27,11 +31,12 @@ declare function require(name: string): any;
 export class DateEditor implements Editor {
   private _$inputWithData: any;
   private _$input: any;
+  private _$editorInputElm: any;
+  private _originalDate: string;
   private _pickerMergedOptions: FlatpickrOption;
 
   flatInstance: any;
   defaultDate: string;
-  originalDate: string;
 
   /** SlickGrid Grid object */
   grid: any;
@@ -100,6 +105,7 @@ export class DateEditor implements Editor {
         altFormat: outputFormat,
         dateFormat: inputFormat,
         closeOnSelect: true,
+        wrap: true,
         locale: (currentLocale !== 'en') ? this.loadFlatpickrLocale(currentLocale) : 'en',
         onChange: () => this.save(),
         errorHandler: () => {
@@ -109,14 +115,25 @@ export class DateEditor implements Editor {
 
       // merge options with optional user's custom options
       this._pickerMergedOptions = { ...pickerOptions, ...(this.editorOptions as FlatpickrOption) };
-      const inputCssClasses = `.editor-text.editor-${columnId}.flatpickr`;
+      const inputCssClasses = `.editor-text.editor-${columnId}.form-control`;
       if (this._pickerMergedOptions.altInput) {
-        this._pickerMergedOptions.altInputClass = 'flatpickr-alt-input editor-text';
+        this._pickerMergedOptions.altInputClass = 'flatpickr-alt-input form-control';
       }
 
-      this._$input = $(`<input type="text" data-defaultDate="${this.defaultDate}" class="${inputCssClasses.replace(/\./g, ' ')}" placeholder="${placeholder}" title="${title}" />`);
-      this._$input.appendTo(this.args.container);
-      this.flatInstance = (flatpickr && this._$input[0] && typeof this._$input[0].flatpickr === 'function') ? this._$input[0].flatpickr(this._pickerMergedOptions) : null;
+      this._$editorInputElm = $(`<div class="flatpickr input-group"></div>`);
+      const closeButtonElm = $(`<span class="input-group-btn" data-clear>
+          <button class="btn btn-default icon-close" type="button"></button>
+        </span>`);
+      this._$input = $(`<input type="text" data-input data-defaultDate="${this.defaultDate}" class="${inputCssClasses.replace(/\./g, ' ')}" placeholder="${placeholder}" title="${title}" />`);
+      this._$input.appendTo(this._$editorInputElm);
+
+      // show clear date button (unless user specifically doesn't want it)
+      if (!this.columnEditor?.params?.hideClearButton) {
+        closeButtonElm.appendTo(this._$editorInputElm);
+      }
+
+      this._$editorInputElm.appendTo(this.args.container);
+      this.flatInstance = (flatpickr && this._$editorInputElm[0] && typeof this._$editorInputElm[0].flatpickr === 'function') ? this._$editorInputElm[0].flatpickr(this._pickerMergedOptions) : flatpickr(this._$editorInputElm, this._pickerMergedOptions as unknown as Partial<FlatpickrBaseOptions>);
 
       // when we're using an alternate input to display data, we'll consider this input as the one to do the focus later on
       // else just use the top one
@@ -132,6 +149,9 @@ export class DateEditor implements Editor {
   destroy() {
     this.hide();
     this._$input.remove();
+    if (this._$editorInputElm?.remove) {
+      this._$editorInputElm.remove();
+    }
     if (this._$inputWithData && typeof this._$inputWithData.remove === 'function') {
       this._$inputWithData.remove();
     }
@@ -189,15 +209,16 @@ export class DateEditor implements Editor {
 
   isValueChanged(): boolean {
     const elmValue = this._$input.val();
-    const inputFormat = mapMomentDateFormatWithFieldType(this.columnEditor.type || (this.columnDef && this.columnDef.type) || FieldType.dateIso);
+    const inputFormat = mapMomentDateFormatWithFieldType(this.columnEditor.type || this.columnDef?.type || FieldType.dateIso);
     const outputTypeFormat = mapMomentDateFormatWithFieldType((this.columnDef && (this.columnDef.outputType || this.columnEditor.type || this.columnDef.type)) || FieldType.dateUtc);
     const elmDateStr = elmValue ? moment(elmValue, inputFormat, false).format(outputTypeFormat) : '';
-    const orgDateStr = this.originalDate ? moment(this.originalDate, inputFormat, false).format(outputTypeFormat) : '';
+    const orgDateStr = this._originalDate ? moment(this._originalDate, inputFormat, false).format(outputTypeFormat) : '';
     if (elmDateStr === 'Invalid date' || orgDateStr === 'Invalid date') {
       return false;
     }
 
-    return (!(elmDateStr === '' && orgDateStr === '')) && (elmDateStr !== orgDateStr);
+    const isChanged = (!(elmDateStr === '' && orgDateStr === '')) && (elmDateStr !== orgDateStr);
+    return isChanged;
   }
 
   loadValue(item: any) {
@@ -208,7 +229,7 @@ export class DateEditor implements Editor {
       const isComplexObject = fieldName && fieldName.indexOf('.') > 0;
       const value = (isComplexObject) ? getDescendantProperty(item, fieldName) : item[fieldName];
 
-      this.originalDate = value;
+      this._originalDate = value;
       this.flatInstance.setDate(value);
     }
   }
