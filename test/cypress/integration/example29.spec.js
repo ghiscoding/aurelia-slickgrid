eb672521cc54620e157f04cea0c0e205537f3907
Vildan Softic <zewa666@gmail.com>
2020-09-25 21:34:00 +0200

    test(example29): added basic integration tests

---
+++ test/cypress/integration/example29.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+describe('Example 29 - Header and Footer slots', () => {
+  it('should display a custom header as slot', () => {
+    cy.visit(`${Cypress.config('baseExampleUrl')}/example29`);
+    cy.get("div[slot='slickgrid-header']")
+      .find('h3')
+      .contains('Grid with header and footer slot');
+  });
+
+  it('should render a footer slot', () => {
+    cy.get("custom-footer[slot='slickgrid-footer']")
+      .should('exist');
+  });
+
+  it('should render a custom element inside footer slot', () => {
+    cy.get("custom-footer[slot='slickgrid-footer']")
+      .find("button")
+      .click()
+      .click()
+      .click()
+      .siblings("div")
+      .should("contain", "3 time(s)");
+  });
+});
