4a4980a268e38d656c59255e5e9dbc830374ccb0
Vildan Softic <vildan.softic@appradar.com>
2020-10-19 16:06:58 +0200

    docs: show ts features on example2

--- src/examples/slickgrid/example2.ts
+++ src/examples/slickgrid/example2.ts
@@ -8,13 +8,26 @@ import {
   SelectedRange,
 } from '../../aurelia-slickgrid';
 
+interface DataItem {
+  id: number;
+  title: string;
+  duration: string;
+  percentComplete: number;
+  percentComplete2: number;
+  start: Date;
+  finish: Date;
+  effortDriven: boolean;
+  phone: string;
+  completed: number;
+}
+
 // create my custom Formatter with the Formatter type
-const myCustomCheckmarkFormatter: Formatter = (row, cell, value, columnDef, dataContext) => {
+const myCustomCheckmarkFormatter: Formatter<DataItem> = (row, cell, value, columnDef, dataContext) => {
   // you can return a string of a object (of type FormatterResultObject), the 2 types are shown below
   return value ? `<i class="fa fa-fire red" aria-hidden="true"></i>` : { text: '<i class="fa fa-snowflake-o" aria-hidden="true"></i>', addClasses: 'lightblue', toolTip: 'Freezing' };
 };
 
-const customEnableButtonFormatter: Formatter = (row: number, cell: number, value: any, columnDef: Column, dataContext: any, grid: any) => {
+const customEnableButtonFormatter: Formatter<DataItem> = (row: number, cell: number, value: any, columnDef: Column, dataContext, grid) => {
   return `<span style="margin-left: 5px">
       <button class="btn btn-xs btn-default">
         <i class="fa ${value ? 'fa-check-circle' : 'fa-circle-thin'} fa-lg" style="color: ${value ? 'black' : 'lavender'}"></i>
@@ -39,7 +52,7 @@ export class Example2 {
 
   aureliaGrid: AureliaGridInstance;
   gridOptions: GridOption;
-  columnDefinitions: Column[];
+  columnDefinitions: Column<DataItem>[];
   dataset: any[];
   resizerPaused = false;
 
@@ -55,6 +68,7 @@ export class Example2 {
 
   /* Define grid Options and Columns */
   defineGrid() {
+    // the columns field property is type-safe, try to add a different string not representing one of DataItems properties
     this.columnDefinitions = [
       { id: 'title', name: 'Title', field: 'title', sortable: true, type: FieldType.string, width: 70 },
       { id: 'phone', name: 'Phone Number using mask', field: 'phone', sortable: true, type: FieldType.number, minWidth: 100, formatter: Formatters.mask, params: { mask: '(000) 000-0000' } },
