841098626d93a0169c13590a2b758bccfca84ada
ghiscoding <gbeaulac@gmail.com>
2020-10-30 23:06:37 -0400

    demo: update github demo page to latest version 2.22.2

---
+++ docs/vendor.font-awesome~f0a7533c.8594887fd2900454be40.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{fxB9:function(n,w,o){}}]);
+//# sourceMappingURL=vendor.font-awesome~f0a7533c.8594887fd2900454be40.bundle.map
---
+++ docs/vendors~cb20e752.ab63a3e3b903089f5b33.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{FsHN:function(n,w,o){}}]);
+//# sourceMappingURL=vendors~cb20e752.ab63a3e3b903089f5b33.bundle.map
