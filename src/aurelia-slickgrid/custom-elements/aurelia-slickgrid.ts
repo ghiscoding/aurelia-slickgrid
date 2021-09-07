de5a906932ed236b128823805d03d8d8e05d58d7
Ghislain B <gbeaulac@gmail.com>
2021-09-07 14:45:35 -0400

    fix(grid): invalidate grid after setItems to re-render grid (#650)
    - fixes a problem only reported in Angular-Slickgrid, not in Aurelia-Slickgrid but might as well apply the same code

--- src/aurelia-slickgrid/custom-elements/aurelia-slickgrid.ts
+++ src/aurelia-slickgrid/custom-elements/aurelia-slickgrid.ts
@@ -819,6 +819,7 @@ export class AureliaSlickgridCustomElement {
         });
         const onSetItemsCalledHandler = dataView.onSetItemsCalled;
         (this._eventHandler as SlickEventHandler<GetSlickEventType<typeof onSetItemsCalledHandler>>).subscribe(onSetItemsCalledHandler, (_e, args) => {
+          grid.invalidate();
           this.handleOnItemCountChanged(dataView.getLength(), args.itemCount);
 
           // when user has resize by content enabled, we'll force a full width calculation since we change our entire dataset
