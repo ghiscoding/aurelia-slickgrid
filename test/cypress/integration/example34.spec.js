d5892b4b2507cde6d06e33b26924868e268d90e7
ghiscoding <gbeaulac@gmail.com>
2021-12-29 22:31:04 -0500

    chore(demo): add Grouping & other extras to Example34

--- test/cypress/integration/example34.spec.js
+++ test/cypress/integration/example34.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Example 34 - Real-Time Stock Trading', { retries: 1 }, () => {
-  const titles = ['Currency', 'Symbol', 'Company', 'Type', 'Change', 'Price', 'Quantity', 'Amount', 'Price History', 'Execution Timestamp'];
+  const titles = ['Currency', 'Symbol', 'Market', 'Company', 'Type', 'Change', 'Price', 'Quantity', 'Amount', 'Price History', 'Execution Timestamp'];
   const GRID_ROW_HEIGHT = 35;
 
   it('should display Example title', () => {
@@ -18,12 +18,33 @@ describe('Example 34 - Real-Time Stock Trading', { retries: 1 }, () => {
 
   it('should check first 5 rows and expect certain data', () => {
     for (let i = 0; i < 5; i++) {
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(0)`).contains(/CAD|USD$/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(3)`).contains(/Buy|Sell$/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(4)`).contains(/\$\(?[0-9\.]*\)?/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(5)`).contains(/\$[0-9\.]*/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(6)`).contains(/\d$/);
-      cy.get(`[style="top:${GRID_ROW_HEIGHT * 0}px"] > .slick-cell:nth(7)`).contains(/\$[0-9\.]*/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(0)`).contains(/CAD|USD$/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(4)`).contains(/Buy|Sell$/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(5)`).contains(/\$\(?[0-9\.]*\)?/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(6)`).contains(/\$[0-9\.]*/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(7)`).contains(/\d$/);
+      cy.get(`[style="top:${GRID_ROW_HEIGHT * i}px"] > .slick-cell:nth(8)`).contains(/\$[0-9\.]*/);
     }
   });
+
+  it('should find multiple green & pink backgrounds to show gains & losses when in real-time mode', () => {
+    cy.get('#refreshRateRange').invoke('val', 5).trigger('change');
+
+    cy.get('.changed-gain').should('have.length.gt', 2);
+    cy.get('.changed-loss').should('have.length.gt', 2);
+  });
+
+  it('should NOT find any green neither pink backgrounds when in real-time is stopped', () => {
+    cy.get('[data-test="highlight-input"]').type('{backspace}{backspace}');
+    cy.get('[data-test="stop-btn"]').click();
+
+    cy.wait(5);
+    cy.get('.changed-gain').should('have.length', 0);
+    cy.get('.changed-loss').should('have.length', 0);
+    cy.wait(1);
+    cy.get('.changed-gain').should('have.length', 0);
+    cy.get('.changed-loss').should('have.length', 0);
+  });
+
+
 });
