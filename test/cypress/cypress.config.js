4e50156f72de7c0cdca1bfdf8d6684baaeec478a
ghiscoding <gbeaulac@gmail.com>
2022-06-09 00:11:13 -0400

    tests: use correct cypress option excludeSpecPattern

--- test/cypress/cypress.config.js
+++ test/cypress/cypress.config.js
@@ -17,7 +17,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:9000/#',
     specPattern: 'e2e/**/*.{js,ts}',
     supportFile: 'support/index.js',
-    ignoreSpecPattern: process.env.CI ? ['node_modules/**/*', '000-update-tests-list.cy.js', '001-run-all-specs.cy.js'] : ['node_modules/**/*'],
+    excludeSpecPattern: process.env.CI ? ['**/node_modules/**', '001-run-all-specs.cy.{js,ts}'] : ['**/node_modules/**'],
     setupNodeEvents(on, config) {
       on("task", {
         updateListOfTests() {
