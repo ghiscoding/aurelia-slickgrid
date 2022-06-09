d02dcd678d2ada4d691e67cce7c717b251ddf99d
Ghislain B <gbeaulac@gmail.com>
2022-06-08 20:33:16 -0400

    chore(tests): follow new naming syntax of Cypress 10 (#820)
    * chore(tests): follow new naming syntax of Cypress 10
    - also add the hack (from this [article](https://medium.com/@darrelfrancis2013/cypress-10-how-to-run-all-tests-despite-the-option-being-removed-725199f4e720)) to add back "Run all Tests" that was removed in Cypress 10 :'(
    - use only available TS Types and `baseUrl` only, in order to stick with Cypress TypeScript interfaces, we shouldn't add any extra properties to the default options object
    
    * chore: use correct cypress project id
    
    * tests: fix cypress baseUrl and SlickGrid examples issue

---
+++ test/cypress/e2e/example15.cy.js
@@ -0,0 +1,505 @@
+/// <reference types="cypress" />
+
+describe('Example 15: Grid State & Presets using Local Storage', { retries: 1 }, () => {
+  const fullEnglishTitles = ['', 'Title', 'Description', 'Duration', '% Complete', 'Start', 'Completed'];
+  const fullFrenchTitles = ['', 'Titre', 'Description', 'Durée', '% Achevée', 'Début', 'Terminé'];
+
+  beforeEach(() => {
+    cy.restoreLocalStorage();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+  });
+
+  it('should display Example title', () => {
+    cy.visit(`${Cypress.config('baseUrl')}/slickgrid/example15`);
+    cy.get('h2').should('contain', 'Example 15: Grid State & Presets using Local Storage');
+
+    cy.clearLocalStorage();
+    cy.get('[data-test=reset-button]').click();
+  });
+
+  it('should reload the page', () => {
+    cy.reload().wait(50);
+  });
+
+  it('should have exact Column Titles in the grid', () => {
+    cy.get('#grid15')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullEnglishTitles[index]));
+  });
+
+  it('should have Pagination displayed with default values', () => {
+    cy.get('#slickGridContainer-grid15').as('grid15');
+
+    // 1st Grid
+    cy.get('@grid15')
+      .find('[data-test=page-number-input]')
+      .invoke('val')
+      .then(pageNumber => expect(pageNumber).to.eq('1'));
+
+    cy.get('@grid15')
+      .find('[data-test=page-count]')
+      .contains('20');
+
+    cy.get('@grid15')
+      .find('[data-test=item-from]')
+      .contains('1');
+
+    cy.get('@grid15')
+      .find('[data-test=item-to]')
+      .contains('25');
+
+    cy.get('@grid15')
+      .find('[data-test=total-items]')
+      .contains('500');
+  });
+
+  it('should drag "Title" column to 3rd position in the grid', () => {
+    const expectedTitles = ['', 'Description', 'Duration', 'Title', '% Complete', 'Start', 'Completed'];
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(1)')
+      .should('contain', 'Title')
+      .trigger('mousedown', 'bottom', { which: 1 });
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .should('contain', 'Duration')
+      .trigger('mousemove', 'bottomRight')
+      .trigger('mouseup', 'bottomRight', { force: true });
+
+    cy.get('#grid15')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(expectedTitles[index]));
+  });
+
+  // --
+  // Cypress does not yet implement the .hover() method and this test won't work until then
+  xit('should resize "Title" column and make it wider', () => {
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .should('contain', 'Title');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .find('.slick-resizable-handle')
+      .trigger('mouseover', -2, 50, { force: true })
+      .should('be.visible')
+      .invoke('show')
+      .hover()
+      .trigger('mousedown', -2, 50, { which: 1, force: true });
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(5)')
+      .trigger('mousemove', 'bottomLeft')
+      .trigger('mouseup', 'bottomLeft', { force: true });
+  });
+
+  it('should hide the "Start" column from the Column Picker', () => {
+    const expectedTitles = ['', 'Description', 'Duration', 'Title', '% Complete', 'Start', 'Completed'];
+
+    cy.get('#grid15')
+      .find('.slick-header-column')
+      .first()
+      .trigger('mouseover')
+      .trigger('contextmenu')
+      .invoke('show');
+
+    cy.get('.slick-column-picker')
+      .find('.slick-column-picker-list')
+      .children()
+      .each(($child, index) => {
+        if (index === 0) {
+          expect($child[0].className).to.eq('hidden');
+          expect($child[0].offsetHeight).to.eq(0);
+          expect($child[0].offsetWidth).to.eq(0);
+        }
+
+        expect($child.text()).to.eq(expectedTitles[index]);
+      });
+
+    cy.get('.slick-column-picker')
+      .find('.slick-column-picker-list')
+      .children('li:nth-child(6)')
+      .children('label')
+      .should('contain', 'Start')
+      .click();
+
+    cy.get('.slick-column-picker:visible')
+      .find('.close')
+      .trigger('click')
+      .click();
+  });
+
+  it('should filter certain tasks', () => {
+    cy.get('.grid-canvas')
+      .find('.slick-row')
+      .should('be.visible');
+
+    cy.get('.filter-title input')
+      .type('Task 1')
+  });
+
+  it('should click on "Title" column to sort it Ascending', () => {
+    const expectedTasks = ['Task 1', 'Task 10', 'Task 100', 'Task 101'];
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .click();
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .find('.slick-sort-indicator.slick-sort-indicator-asc')
+      .should('be.visible');
+
+    cy.get('#grid15')
+      .find('.slick-row')
+      .each(($row, index) => {
+        if (index > expectedTasks.length - 1) {
+          return;
+        }
+        cy.wrap($row).children('.slick-cell:nth(3)')
+          .should('contain', expectedTasks[index]);
+      });
+  });
+
+  it('should hover over the "Duration" column click on "Sort Descending" command', () => {
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(2)')
+      .trigger('mouseover')
+      .children('.slick-header-menu-button')
+      .should('be.hidden')
+      .invoke('show')
+      .click();
+
+    cy.get('.slick-header-menu')
+      .should('be.visible')
+      .children('.slick-menu-item:nth-of-type(5)')
+      .children('.slick-menu-content')
+      .should('contain', 'Sort Descending')
+      .click();
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(2)')
+      .find('.slick-sort-indicator.slick-sort-indicator-desc')
+      .should('be.visible');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(2)')
+      .find('.slick-sort-indicator-numbered')
+      .should('be.visible')
+      .should('contain', '2');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .find('.slick-sort-indicator-numbered')
+      .should('be.visible')
+      .should('contain', '1');
+  });
+
+  it('should select row (Task 105)', () => {
+    cy.get('#grid15')
+      .contains('Task 105')
+      .parent()
+      .children('.slick-cell-checkboxsel')
+      .find('input[type=checkbox]')
+      .click({ force: true });
+  });
+
+  it('should change Page Size and Page Number then expect the Pagination to have correct values', () => {
+    const expectedTasks = ['Task 135', 'Task 136', 'Task 137', 'Task 138', 'Task 139', 'Task 14'];
+
+    cy.get('#slickGridContainer-grid15').as('grid15');
+
+    cy.get('@grid15')
+      .find('#items-per-page-label').select('20');
+
+    cy.get('@grid15')
+    cy.get('.icon-seek-next').click().click();
+
+    cy.wait(100);
+
+    cy.get('@grid15')
+      .find('[data-test=page-number-input]')
+      .invoke('val')
+      .then(pageNumber => expect(pageNumber).to.eq('3'));
+
+    cy.get('@grid15')
+      .find('[data-test=page-count]')
+      .contains('6');
+
+    cy.get('@grid15')
+      .find('[data-test=item-from]')
+      .contains('41');
+
+    cy.get('@grid15')
+      .find('[data-test=item-to]')
+      .contains('60');
+
+    cy.get('@grid15')
+      .find('[data-test=total-items]')
+      .contains('111');
+
+    cy.get('@grid15')
+      .find('.slick-row')
+      .each(($row, index) => {
+        if (index > expectedTasks.length - 1) {
+          return;
+        }
+        cy.wrap($row).children('.slick-cell:nth(3)')
+          .should('contain', expectedTasks[index]);
+      });
+  });
+
+  it('should select row (Task 144)', () => {
+    cy.get('#grid15')
+      .contains('Task 144')
+      .parent()
+      .children('.slick-cell-checkboxsel')
+      .find('input[type=checkbox]')
+      .click({ force: true });
+  });
+
+  it('should reload the page', () => {
+    cy.reload().wait(50);
+  });
+
+  it('should expect the same Grid State to persist after the page got reloaded', () => {
+    const expectedTitles = ['', 'Description', 'Duration', 'Title', '% Complete', 'Completed'];
+
+    cy.get('#grid15')
+      .find('.grid-canvas')
+      .find('.slick-row')
+      .should('be.visible');
+
+    cy.get('#grid15')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.find('.slick-column-name').text()).to.eq(expectedTitles[index]));
+  });
+
+  it('should expect the same Pagination to persist after reload', () => {
+    const expectedTasks = ['Task 135', 'Task 136', 'Task 137', 'Task 138', 'Task 139', 'Task 14'];
+
+    cy.get('#slickGridContainer-grid15').as('grid15');
+
+    cy.get('@grid15')
+      .find('[data-test=page-number-input]')
+      .invoke('val')
+      .then(pageNumber => expect(pageNumber).to.eq('3'));
+
+    cy.get('@grid15')
+      .find('[data-test=page-count]')
+      .contains('6');
+
+    cy.get('@grid15')
+      .find('[data-test=item-from]')
+      .contains('41');
+
+    cy.get('@grid15')
+      .find('[data-test=item-to]')
+      .contains('60');
+
+    cy.get('@grid15')
+      .find('[data-test=total-items]')
+      .contains('111');
+
+    cy.get('@grid15')
+      .find('.slick-row')
+      .each(($row, index) => {
+        if (index > expectedTasks.length - 1) {
+          return;
+        }
+        cy.wrap($row).children('.slick-cell:nth(3)')
+          .should('contain', expectedTasks[index]);
+      });
+  });
+
+  it('should expect row selection (Task 144) to be persisted', () => {
+    cy.get('#grid15')
+      .contains('Task 144')
+      .parent()
+      .children()
+      .each($child => {
+        console.log($child)
+        expect($child.attr('class')).to.contain('selected');
+      });
+  });
+
+  it('should have French titles in Column Picker after switching to Language', () => {
+    const expectedTitles = ['', 'Description', 'Durée', 'Titre', '% Achevée', 'Début', 'Terminé'];
+
+    cy.get('[data-test=language-button]')
+      .click();
+
+    cy.get('[data-test=selected-locale]')
+      .should('contain', 'fr.json');
+
+    cy.get('#grid15')
+      .find('.slick-header-column')
+      .first()
+      .trigger('mouseover')
+      .trigger('contextmenu')
+      .invoke('show');
+
+    cy.get('.slick-column-picker')
+      .find('.slick-column-picker-list')
+      .children()
+      .each(($child, index) => {
+        if (index === 0) {
+          expect($child[0].className).to.eq('hidden');
+          expect($child[0].offsetHeight).to.eq(0);
+          expect($child[0].offsetWidth).to.eq(0);
+        }
+
+        expect($child.text()).to.eq(expectedTitles[index]);
+      });
+
+    cy.get('.slick-column-picker:visible')
+      .find('.close')
+      .trigger('click')
+      .click();
+  });
+
+  it('should have French titles in Grid Menu after switching to Language', () => {
+    const expectedTitles = ['', 'Description', 'Durée', 'Titre', '% Achevée', 'Début', 'Terminé'];
+
+    cy.get('#grid15')
+      .find('button.slick-grid-menu-button')
+      .trigger('click')
+      .click();
+
+    cy.get('.slick-grid-menu')
+      .find('.slick-column-picker-list')
+      .children()
+      .each(($child, index) => {
+        if (index === 0) {
+          expect($child[0].className).to.eq('hidden');
+          expect($child[0].offsetHeight).to.eq(0);
+          expect($child[0].offsetWidth).to.eq(0);
+        }
+
+        expect($child.text()).to.eq(expectedTitles[index]);
+      });
+
+    cy.get('.slick-grid-menu:visible')
+      .find('.close')
+      .trigger('click')
+      .click();
+  });
+
+  it('should hover over the "Terminé" column and click on "Cacher la colonne" remove the column from grid', () => {
+    const expectedTitles = ['', 'Description', 'Durée', 'Titre', '% Achevée'];
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(5)')
+      .trigger('mouseover')
+      .children('.slick-header-menu-button')
+      .should('be.hidden')
+      .invoke('show')
+      .click();
+
+    cy.get('.slick-header-menu')
+      .should('be.visible')
+      .children('.slick-menu-item:nth-of-type(9)')
+      .children('.slick-menu-content')
+      .should('contain', 'Cacher la colonne')
+      .click();
+
+    cy.get('#grid15')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.find('.slick-column-name').text()).to.eq(expectedTitles[index]));
+  });
+
+  it('should be able to freeze "Description" column', () => {
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(1)')
+      .trigger('mouseover')
+      .children('.slick-header-menu-button')
+      .should('be.hidden')
+      .invoke('show')
+      .click();
+
+    cy.get('.slick-header-menu')
+      .should('be.visible')
+      .children('.slick-menu-item:nth-of-type(1)')
+      .children('.slick-menu-content')
+      .should('contain', 'Geler les colonnes')
+      .click();
+  });
+
+  it('should reload the page', () => {
+    cy.reload().wait(50);
+  });
+
+  it('should expect the same Grid State to persist after the page got reloaded, however we always load in English', () => {
+    const expectedTitles = ['', 'Description', 'Duration', 'Title', '% Complete'];
+
+    cy.get('#grid15')
+      .find('.grid-canvas')
+      .find('.slick-row')
+      .should('be.visible');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(2)')
+      .find('.slick-sort-indicator-numbered')
+      .should('be.visible')
+      .should('contain', '2');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .find('.slick-sort-indicator.slick-sort-indicator-asc')
+      .should('be.visible');
+
+    cy.get('.slick-header-columns')
+      .children('.slick-header-column:nth(3)')
+      .find('.slick-sort-indicator-numbered')
+      .should('be.visible')
+      .should('contain', '1');
+
+    cy.get('#grid15')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.find('.slick-column-name').text()).to.eq(expectedTitles[index]));
+  });
+
+  it('should expect row selection (Task 144) to be persisted', () => {
+    cy.get('#grid15')
+      .contains('Task 144')
+      .parent()
+      .children()
+      .each($child => {
+        console.log($child)
+        expect($child.attr('class')).to.contain('selected');
+      });
+  });
+
+  it('should go back to first page and expect row selection (Task 105) to be persisted', () => {
+    cy.get('#slickGridContainer-grid15').as('grid15');
+
+    cy.get('@grid15')
+      .find('.icon-seek-first')
+      .click()
+      .wait(10);
+
+    cy.get('#grid15')
+      .contains('Task 105')
+      .parent()
+      .children()
+      .each($child => {
+        console.log($child)
+        expect($child.attr('class')).to.contain('selected');
+      });
+  });
+
+  it('should have a persisted frozen column after "Description" and a grid with 4 containers on page load with 2 columns on the left and 3 columns on the right', () => {
+    cy.get('[style="top:0px"]').should('have.length', 2);
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 3);
+  });
+});
---
+++ test/cypress/e2e/example20.cy.js
@@ -0,0 +1,224 @@
+/// <reference types="cypress" />
+
+describe('Example 20 - Frozen Grid', { retries: 1 }, () => {
+  // NOTE:  everywhere there's a * 2 is because we have a top+bottom (frozen rows) containers even after Unfreeze Columns/Rows
+
+  const fullTitles = ['#', 'Title', '% Complete', 'Start', 'Finish', 'Cost | Duration', 'Effort Driven', 'Title 1', 'Title 2', 'Title 3', 'Title 4'];
+
+  it('should display Example title', () => {
+    cy.visit(`${Cypress.config('baseUrl')}/slickgrid/example20`);
+    cy.get('h2').should('contain', 'Example 20: Pinned (frozen) Columns/Rows');
+  });
+
+  it('should have exact column titles on 1st grid', () => {
+    cy.get('#grid20')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+  });
+
+  it('should have exact Column Header Titles in the grid', () => {
+    cy.get('#grid20')
+      .find('.slick-header-columns:nth(0)')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+  });
+
+  it('should have a frozen grid with 4 containers on page load with 3 columns on the left and 4 columns on the right', () => {
+    cy.get('[style="top:0px"]').should('have.length', 2 * 2);
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 3 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '0');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+
+  it('should hide "Title" column from Grid Menu and expect last frozen column to be "% Complete"', () => {
+    const newColumnList = ['#', '% Complete', 'Start', 'Finish', 'Cost | Duration', 'Effort Driven', 'Title 1', 'Title 2', 'Title 3', 'Title 4'];
+
+    cy.get('#grid20')
+      .find('button.slick-grid-menu-button')
+      .click({ force: true });
+
+    cy.get('#grid20')
+      .get('.slick-grid-menu:visible')
+      .find('.slick-column-picker-list')
+      .children('li:visible:nth(1)')
+      .children('label')
+      .should('contain', 'Title')
+      .click({ force: true });
+
+    cy.get('#grid20')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(newColumnList[index]));
+
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 2 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+  it('should show again "Title" column from Grid Menu and expect last frozen column to still be "% Complete"', () => {
+    cy.get('#grid20')
+      .get('.slick-grid-menu:visible')
+      .find('.slick-column-picker-list')
+      .children('li:visible:nth(1)')
+      .children('label')
+      .should('contain', 'Title')
+      .click({ force: true });
+
+    cy.get('#grid20')
+      .get('.slick-grid-menu:visible')
+      .find('.close')
+      .click({ force: true });
+
+    cy.get('#grid20')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 3 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+  it('should hide "Title" column from Header Menu and expect last frozen column to be "% Complete"', () => {
+    const newColumnList = ['#', '% Complete', 'Start', 'Finish', 'Cost | Duration', 'Effort Driven', 'Title 1', 'Title 2', 'Title 3', 'Title 4'];
+
+    cy.get('#grid20')
+      .find('.slick-header-column:nth(1)')
+      .trigger('mouseover')
+      .children('.slick-header-menu-button')
+      .should('be.hidden')
+      .invoke('show')
+      .click();
+
+    cy.get('.slick-header-menu')
+      .should('be.visible')
+      .children('.slick-menu-item:nth-of-type(8)')
+      .children('.slick-menu-content')
+      .should('contain', 'Hide Column')
+      .click();
+
+    cy.get('#grid20')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(newColumnList[index]));
+
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 2 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+  it('should show again "Title" column from Column Picker and expect last frozen column to still be "% Complete"', () => {
+    cy.get('#grid20')
+      .find('.slick-header-column:nth(5)')
+      .trigger('mouseover')
+      .trigger('contextmenu')
+      .invoke('show');
+
+    cy.get('.slick-column-picker')
+      .find('.slick-column-picker-list')
+      .children('li:nth-child(2)')
+      .children('label')
+      .should('contain', 'Title')
+      .click();
+
+    cy.get('.slick-column-picker:visible')
+      .find('.close')
+      .trigger('click')
+      .click();
+
+    cy.get('#grid20')
+      .find('.slick-header-columns')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 3 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+  it('should click on the "Remove Frozen Columns" button to switch to a regular grid without frozen columns and expect 7 columns on the left container', () => {
+    cy.get('[data-test=remove-frozen-column-button]')
+      .click({ force: true });
+
+    cy.get('[style="top:0px"]').should('have.length', 1 * 2);
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 11 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '0');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(3)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(4)').should('contain', '2009-05-05');
+  });
+
+  it('should have exact Column Header Titles in the grid', () => {
+    cy.get('#grid20')
+      .find('.slick-header-columns:nth(0)')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+  });
+
+  it('should click on the "Set 3 Frozen Columns" button to switch frozen columns grid and expect 3 frozen columns on the left and 4 columns on the right', () => {
+    cy.get('[data-test=set-3frozen-columns]')
+      .click({ force: true });
+
+    cy.get('[style="top:0px"]').should('have.length', 2 * 2);
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 3 * 2);
+    cy.get('.grid-canvas-right > [style="top:0px"]').children().should('have.length', 8 * 2);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '0');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-right > [style="top:0px"] > .slick-cell:nth(1)').should('contain', '2009-05-05');
+  });
+
+  it('should have exact Column Header Titles in the grid', () => {
+    cy.get('#grid20')
+      .find('.slick-header-columns:nth(0)')
+      .children()
+      .each(($child, index) => expect($child.text()).to.eq(fullTitles[index]));
+  });
+
+  it('should click on the Grid Menu command "Unfreeze Columns/Rows" to switch to a regular grid without frozen columns and expect 7 columns on the left container', () => {
+    cy.get('#grid20')
+      .find('button.slick-grid-menu-button')
+      .click({ force: true });
+
+    cy.contains('Unfreeze Columns/Rows')
+      .click({ force: true });
+
+    cy.get('[style="top:0px"]').should('have.length', 1);
+    cy.get('.grid-canvas-left > [style="top:0px"]').children().should('have.length', 11);
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(0)').should('contain', '0');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(1)').should('contain', 'Task 0');
+
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(3)').should('contain', '2009-01-01');
+    cy.get('.grid-canvas-left > [style="top:0px"] > .slick-cell:nth(4)').should('contain', '2009-05-05');
+  });
+});
