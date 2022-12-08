2f52047a34887361c13d99dd76488798295bd973
ghiscoding <gbeaulac@gmail.com>
2022-12-07 19:01:41 -0500

    tests: lower Cypress mem usage to avoid UI crashing

--- test/cypress/cypress.config.js
+++ test/cypress/cypress.config.js
@@ -9,6 +9,7 @@ module.exports = defineConfig({
   projectId: 'gtbpy4',
   screenshotsFolder: 'screenshots',
   videosFolder: 'videos',
+  numTestsKeptInMemory: 5,
   retries: {
     runMode: 2,
     openMode: 0,
