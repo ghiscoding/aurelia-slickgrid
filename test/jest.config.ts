3a18ebec12fcb3280710b8dc887be605cc811292
ghiscoding <gbeaulac@gmail.com>
2023-11-11 13:41:44 -0500

    chore: fix failing unit tests with default imports

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -10,12 +10,16 @@ const config: Config.InitialOptions = {
     '!src/assets/**',
     '!src/**/models/**',
     '!**/node_modules/**',
+    '!**/models/**',
     '!**/test/**',
     '!src/examples/**',
   ],
   coverageDirectory: '<rootDir>/test/jest-coverage',
   coveragePathIgnorePatterns: [
     'example-data.js',
+    'constants.ts',
+    'index.ts',
+    'slickgrid-config.ts',
     'global-grid-options.ts',
     '\\.d\\.ts$',
     '<rootDir>/node_modules/'
