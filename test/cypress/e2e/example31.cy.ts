8dfbbf75f3abf69807806cf6062794b627ea2498
ghiscoding <gbeaulac@gmail.com>
2024-01-21 00:24:15 -0500

    chore: fix failing Cypress E2E test

--- test/cypress/e2e/example31.cy.ts
+++ test/cypress/e2e/example31.cy.ts
@@ -693,7 +693,7 @@ describe('Example 31 - OData Grid using RxJS', () => {
 
       cy.get(`[style="top: ${GRID_ROW_HEIGHT * 0}px;"] > .slick-cell:nth(2)`)
         .should('contain', 'male')
-        .click();
+        .type('{enter}');
 
       cy.get('[data-name="editor-gender"].ms-drop')
         .find('li:visible')
