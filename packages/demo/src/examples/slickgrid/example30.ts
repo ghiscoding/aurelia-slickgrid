36c6a3fea45bafd1dc42d19322d180e252f3acef
ghiscoding <gbeaulac@gmail.com>
2024-03-12 22:20:34 -0400

    docs: add more docs about Toggle Dark Mode in Grid Menu

--- packages/demo/src/examples/slickgrid/example30.ts
+++ packages/demo/src/examples/slickgrid/example30.ts
@@ -438,7 +438,7 @@ export class Example30 {
       // when using the cellMenu, you can change some of the default options and all use some of the callback methods
       enableCellMenu: true,
       gridMenu: {
-        hideToggleDarkModeCommand: false, // disabled command by default
+        hideToggleDarkModeCommand: false, // hidden by default
         onCommand: (_, args) => {
           if (args.command === 'toggle-dark-mode') {
             this._darkMode = !this._darkMode; // keep local toggle var in sync
