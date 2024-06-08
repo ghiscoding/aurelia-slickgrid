7f8199042114ca9b7f08a9a357bd3fce864a2db7
Ghislain B <gbeaulac@gmail.com>
2024-06-07 21:47:17 -0400

    chore: test Date Filter should trigger Grid State change with Backspace (#1212)

--- test/cypress/e2e/example04.cy.ts
+++ test/cypress/e2e/example04.cy.ts
@@ -1,8 +1,14 @@
 import { isAfter, isBefore, isEqual, parse } from '@formkit/tempo';
-
 import { removeExtraSpaces } from '../plugins/utilities';
 
 describe('Example 4 - Client Side Sort/Filter Grid', () => {
+  beforeEach(() => {
+    // create a console.log spy for later use
+    cy.window().then((win) => {
+      cy.spy(win.console, 'log');
+    });
+  });
+
   it('should display Example title', () => {
     cy.visit(`${Cypress.config('baseUrl')}/example4`);
     cy.get('h2').should('contain', 'Example 4: Client Side Sort/Filter');
@@ -11,16 +17,6 @@ describe('Example 4 - Client Side Sort/Filter Grid', () => {
   describe('Load Grid with Presets', () => {
     const presetDurationValues = [98, 10];
     const presetUsDateShort = '4/20/25';
-
-    it('should have some metrics shown in the grid footer but make sure the first number is below 1500 items', () => {
-      cy.get('#slickGridContainer-grid4')
-        .find('.slick-custom-footer')
-        .find('.right-footer')
-        .should($span => {
-          const text = removeExtraSpaces($span.text()); // remove all white spaces
-          expect(text).not.to.eq('1500 of 1500 items');
-        });
-    });
 
     it('should have "Duration" fields within the inclusive range of the preset filters and be displayed in the Filter itself', () => {
       cy.get('.ms-filter.search-filter.filter-duration.filled')
@@ -153,7 +149,7 @@ describe('Example 4 - Client Side Sort/Filter Grid', () => {
           cy.wrap($row)
             .children('.slick-cell:nth(4)')
             .each(($cell) => {
-              const isDateValid = isEqual($cell.text(), dynamicStartDate) || isAfter($cell.text(), dynamicStartDate);
+              const isDateValid = isEqual(parse($cell.text()), dynamicStartDate) || isAfter(parse($cell.text()), dynamicStartDate);
               expect(isDateValid).to.eq(true);
             });
         });
@@ -211,6 +207,73 @@ describe('Example 4 - Client Side Sort/Filter Grid', () => {
         .last()
         .children('.slick-cell:nth(4)')
         .should('contain', '');
+    });
+  });
+
+  describe('Grid State Changes', () => {
+    const dynamicStartDate = '2001-02-28';
+
+    it('should click on Set Dynamic Filters', () => {
+      cy.get('[data-test=set-dynamic-filter]')
+        .click();
+    });
+
+    it('should have Start Date within the range of the dynamic filters', () => {
+      cy.get('.search-filter.filter-start')
+        .find('input')
+        .invoke('val')
+        .then(text => expect(text).to.eq(dynamicStartDate));
+
+      cy.get('#grid4')
+        .find('.slick-row')
+        .each(($row) => {
+          cy.wrap($row)
+            .children('.slick-cell:nth(4)')
+            .each(($cell) => {
+              const isDateValid = isEqual(parse($cell.text()), dynamicStartDate) || isAfter(parse($cell.text()), dynamicStartDate);
+              expect(isDateValid).to.eq(true);
+            });
+        });
+    });
+
+    it('should focus on Start filter, then type Backspace and expect Start filter to no longer exists in the list of Filters in Grid State change', () => {
+      cy.get('.search-filter.filter-start').click();
+      cy.wait(20);
+      cy.get('.search-filter.filter-start input.date-picker').type('{backspace}', { force: true });
+
+      cy.get('.search-filter.filter-start')
+        .find('input')
+        .invoke('val')
+        .then(text => expect(text).to.eq(''));
+
+      cy.window().then((win) => {
+        expect(win.console.log).to.have.callCount(1);
+        expect(win.console.log).to.be.calledWith('Client sample, Grid State changed:: ', {
+          newValues: [
+            { columnId: 'duration', searchTerms: ['2', '25', '48', '50'], operator: 'IN' },
+            { columnId: 'complete', searchTerms: ['95'], operator: '<' },
+            { columnId: 'effort-driven', searchTerms: ['true'], operator: 'EQ' }
+          ], type: 'filter'
+        });
+      });
+    });
+
+    it('should click on DOM body and reopen Start filter date picker and still expect it to be empty', () => {
+      cy.get('h2').click(); // just to simulate clicking outside of the date picker
+
+      cy.get('.search-filter.filter-start')
+        .find('input')
+        .invoke('val')
+        .then(text => expect(text).to.eq(''));
+
+      cy.get('.search-filter.filter-start')
+        .click();
+
+      cy.get('.vanilla-calendar:visible')
+        .find('.vanilla-calendar-day__btn_selected')
+        .should('not.exist');
+
+      cy.get('h2').click();
     });
   });
 });
