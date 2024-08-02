e19552caa2d6ab9edfa67e39781c8a8b3f00115c
ghiscoding <gbeaulac@gmail.com>
2024-08-01 23:59:12 -0400

    chore: fix failing Cypress test because of 1st day of month

--- packages/demo/src/examples/slickgrid/example23.ts
+++ packages/demo/src/examples/slickgrid/example23.ts
@@ -156,7 +156,7 @@ export class Example23 {
 
     const today = new Date();
     const presetLowestDay = format(addDay(new Date(), -2), 'YYYY-MM-DD');
-    const presetHighestDay = format(addDay(new Date(), today.getDate() < 14 ? 30 : 25), 'YYYY-MM-DD');
+    const presetHighestDay = format(addDay(new Date(), today.getDate() < 14 ? 28 : 25), 'YYYY-MM-DD');
 
     this.gridOptions = {
       autoResize: {
