7d01e51de3324ce4e7f767ac0efd7559c65badf4
ghiscoding <gbeaulac@gmail.com>
2024-10-17 20:52:01 -0400

    feat: allow providing a Custom Pagination Component

--- packages/aurelia-slickgrid/src/models/aureliaGridInstance.interface.ts
+++ packages/aurelia-slickgrid/src/models/aureliaGridInstance.interface.ts
@@ -1,5 +1,6 @@
 import type {
   BackendService,
+  BasePaginationComponent,
   ExtensionService,
   FilterService,
   GridEventService,
@@ -53,6 +54,9 @@ export interface AureliaGridInstance {
 
   /** Grouping (and colspan) Service */
   groupingService: GroupingAndColspanService;
+
+  /** Pagination Component */
+  paginationComponent?: BasePaginationComponent;
 
   /** Pagination Service (allows you to programmatically go to first/last page, etc...) */
   paginationService?: PaginationService;
