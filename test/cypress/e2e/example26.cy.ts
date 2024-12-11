b59bf34e62173d5a18a98efc38d88307e12bace6
Ghislain B. <gbeaulac@gmail.com>
2024-12-10 22:06:27 -0500

    chore(vue): add more E2E tests (#1297)

--- test/cypress/e2e/example26.cy.ts
+++ test/cypress/e2e/example26.cy.ts
@@ -24,16 +24,19 @@ describe('Example 26 - Use of Aurelia Custom Elements', () => {
     cy.get('.slick-cell select.form-control')
       .select('John');
 
+    cy.get('[data-test="auto-edit-checkbox"]')
+      .check();
+
     cy.get('.slick-row:nth(1)')
       .children('.slick-cell:nth(1)')
       .click();
 
     cy.get('.slick-cell select.form-control')
-      .select('Pierre');
+      .select('Paul');
   });
 
-  it('should have 2 first rows with "Assignee" of "John" (0) then "Pierre" (1)', () => {
-    const tasks = [['Task 0', 'John', 'John'], ['Task 1', 'Pierre', 'Pierre']];
+  it('should have 2 first rows with "Assignee" of "John" (0) then "Paul" (1)', () => {
+    const tasks = [['Task 0', 'John', 'John'], ['Task 1', 'Paul', 'Paul']];
 
     cy.get('#grid26')
       .find('.slick-row')
@@ -49,5 +52,35 @@ describe('Example 26 - Use of Aurelia Custom Elements', () => {
           cy.wrap($row).children('.slick-cell:nth(2)').should('contain', tasks[index][2]);
         }
       });
+  });
+
+  it('should filter Assignee and expect only 1 row with Paul', () => {
+    cy.get('#grid26')
+      .find('.slick-headerrow-column:nth(1)')
+      .find('select.form-control')
+      .select('Paul');
+
+    cy.get('#grid26')
+      .find('.slick-row')
+      .children('.slick-cell:nth(1)')
+      .should('contain', 'Paul');
+
+    cy.get('#grid26')
+      .find('.slick-row')
+      .should('have.length', 1);
+  });
+
+  it('should be able to clear the filter and expect more than 1 row in the grid', () => {
+    cy.get('[data-test="clear-filters"]')
+      .click();
+
+    cy.get('#grid26')
+      .find('.slick-row')
+      .children('.slick-cell:nth(1)')
+      .should('not.contain', 'Paul');
+
+    cy.get('#grid26')
+      .find('.slick-row')
+      .should('have.length.greaterThan', 1);
   });
 });
