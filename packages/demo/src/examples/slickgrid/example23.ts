6fc0a474d300887588785d3017bc7b8200d1b517
ghiscoding <gbeaulac@gmail.com>
2025-02-02 00:40:49 -0500

    chore: fix failing Cypress test

--- packages/demo/src/examples/slickgrid/example23.ts
+++ packages/demo/src/examples/slickgrid/example23.ts
@@ -154,9 +154,8 @@ export class Example23 {
       }
     ];
 
-    const today = new Date();
     const presetLowestDay = format(addDay(new Date(), -2), 'YYYY-MM-DD');
-    const presetHighestDay = format(addDay(new Date(), today.getDate() < 14 ? 28 : 25), 'YYYY-MM-DD');
+    const presetHighestDay = format(addDay(new Date(), 25), 'YYYY-MM-DD');
 
     this.gridOptions = {
       autoResize: {
