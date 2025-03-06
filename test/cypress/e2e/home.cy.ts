831426851db6b9707022e446bd4825da921b8793
ghiscoding <gbeaulac@gmail.com>
2025-03-06 00:45:40 -0500

    chore: update home page & tests

--- test/cypress/e2e/home.cy.ts
+++ test/cypress/e2e/home.cy.ts
@@ -2,14 +2,6 @@ describe('Home Page', () => {
   it('should display Home Page', () => {
     cy.visit(`${Cypress.config('baseUrl')}/home`);
 
-    cy.get('h1').should(($h1) => {
-      expect($h1, 'text content').to.have.text('Aurelia-Slickgrid');
-    });
-
-    cy.get('h3')
-      .contains('Introduction');
-
-    cy.get('h3')
-      .contains('Wiki / Documentation');
+    cy.get('h1').should('have.text', 'Aurelia-Slickgrid');
   });
 });
