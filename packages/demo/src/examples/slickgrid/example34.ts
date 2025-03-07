270896ef43966491b39a2e93441fbe9182bf30a4
ghiscoding <gbeaulac@gmail.com>
2025-03-07 10:36:14 -0500

    chore: disable `resizer.autoHeight` for trading demo [refresh gh-pages]

--- packages/demo/src/examples/slickgrid/example34.ts
+++ packages/demo/src/examples/slickgrid/example34.ts
@@ -78,14 +78,13 @@ const historicSparklineFormatter: Formatter = (_row, _cell, _value: string, _col
 export class Example34 {
   private _darkMode = false;
   title = 'Example 34: Real-Time Trading Platform';
-  subTitle = `Simulate a stock trading platform with lot of price changes
-  <ul>
-    <li>you can start/stop the simulation</li>
-    <li>optionally change random numbers, between 0 and 10 symbols, per cycle (higher numbers means more changes)</li>
-    <li>optionally change the simulation changes refresh rate in ms (lower number means more changes).</li>
-    <li>you can Group by 1 of these columns: Currency, Market or Type</li>
-    <li>to show SlickGrid HUGE PERF., do the following: (1) lower Changes Rate (2) increase both Changes per Cycle and (3) lower Highlight Duration</li>
-  </ul>`;
+  subTitle = `Simulate a stock trading platform with lot of price changes, it is strongly suggested to disable the <code>autoResize.autoHeight</code> grid option for this type of grid.
+    <ul>
+      <li>you can start/stop the simulation, you can see SlickGrid huge perf by setting: (1) lower Changes Rate, (2) increase both Changes per Cycle, and (3) lower Highlight Duration</li>
+      <li>optionally change random numbers, between 0 and 10 symbols, per cycle (higher numbers means more changes)</li>
+      <li>optionally change the simulation changes refresh rate in ms (lower number means more changes).</li>
+      <li>you can Group by 1 of these columns: Currency, Market or Type</li>
+    </ul>`;
 
   aureliaGrid!: AureliaGridInstance;
   gridOptions!: GridOption;
