81b9630c2302f58c69acd7de067907c8e0d9c61f
Ghislain B. <gbeaulac@gmail.com>
2025-03-19 11:11:45 -0400

    chore: remove unused code & simplify code

--- packages/aurelia-slickgrid/src/services/aureliaUtil.service.ts
+++ packages/aurelia-slickgrid/src/services/aureliaUtil.service.ts
@@ -2,29 +2,26 @@ import { AppTask, type Constructable, CustomElement, IAurelia, resolve, singleto
 
 import type { AureliaViewOutput, ViewModelBindableInputData } from '../models/index';
 
-(IAurelia as any).test = 'import 1';
-
 @singleton()
 export class AureliaUtilService {
   constructor(private readonly au: IAurelia = resolve(IAurelia)) { }
 
   async createAureliaViewModelAddToSlot(viewModel: Constructable, bindableData?: ViewModelBindableInputData, targetElement?: HTMLElement): Promise<AureliaViewOutput | null> {
-    if (!targetElement) {
-      return null;
+    if (targetElement) {
+      const def = CustomElement.getDefinition(viewModel);
+      const addonBindable = bindableData?.addon ? 'addon.bind="bindableData.addon"' : '';
+      const gridBindable = bindableData?.grid ? 'grid.bind="bindableData.grid"' : '';
+      const dataViewBindable = bindableData?.dataView ? 'data-view.bind="bindableData.dataView"' : '';
+      const parentBindable = bindableData?.parent ? 'parent.bind="bindableData.parent"' : '';
+  
+      targetElement.innerHTML = `<${def.name} model.bind="bindableData.model" ${addonBindable} ${gridBindable} ${dataViewBindable} ${parentBindable}></${def.name}>`.trim();
+  
+      return await this.au.enhance({
+        host: targetElement,
+        component: { bindableData },
+        container: this.au.container.createChild().register(AppTask.creating(() => { }))
+      });
     }
-
-    const def = CustomElement.getDefinition(viewModel);
-    const addonBindable = bindableData?.addon ? 'addon.bind="bindableData.addon"' : '';
-    const gridBindable = bindableData?.grid ? 'grid.bind="bindableData.grid"' : '';
-    const dataViewBindable = bindableData?.dataView ? 'data-view.bind="bindableData.dataView"' : '';
-    const parentBindable = bindableData?.parent ? 'parent.bind="bindableData.parent"' : '';
-
-    targetElement.innerHTML = `<${def.name} model.bind="bindableData.model" ${addonBindable} ${gridBindable} ${dataViewBindable} ${parentBindable}></${def.name}>`.trim();
-
-    return await this.au.enhance({
-      host: targetElement,
-      component: { bindableData },
-      container: this.au.container.createChild().register(AppTask.creating(() => { }))
-    });
+    return null;
   }
 }
